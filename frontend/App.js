a603431f84827ba4d1a555b9b4ef09c2a8523d6a
kganzorig <khusy.gan@gmail.com>
2023-07-25 11:20:48 +0100

    imports on App.js

--- frontend/App.js
+++ frontend/App.js
@@ -1,5 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import MapView from 'react-native-maps';
+import { createStackNavigator } from '@react-navigation/stack';
 
 export default function App() {
   return (
