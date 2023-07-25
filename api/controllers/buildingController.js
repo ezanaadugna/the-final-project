25993f62e9282de617d205aef50203b32a4595b9
kganzorig <khusy.gan@gmail.com>
2023-07-25 13:45:53 +0100

    Setting up maps backend files

---
+++ api/controllers/buildingController.js
@@ -0,0 +1,5 @@
+const BuildingController = {
+
+};
+
+module.exports = BuildingController;
---
+++ api/routes/buildingRoutes.js
@@ -0,0 +1,5 @@
+const express = require("express");
+const router = express.Router();
+const BuildingController = require("../controllers/BuildingController");
+
+module.exports = router;
