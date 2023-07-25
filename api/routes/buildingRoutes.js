436018fa947d0b7035ff6e12221fc5cc079e3edc
kganzorig <khusy.gan@gmail.com>
2023-07-25 13:47:46 +0100

    corrected route in routes

--- api/routes/buildingRoutes.js
+++ api/routes/buildingRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
 const router = express.Router();
-const BuildingController = require("../controllers/BuildingController");
+const BuildingController = require("../controllers/buildingController");
 
 module.exports = router;
