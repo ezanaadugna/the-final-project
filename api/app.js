3ba83509abb859ec9373671ba67a7793a4fc1375
kganzorig <khusy.gan@gmail.com>
2023-07-26 13:49:23 +0100

    routes for api buildingController added onto app.js

--- api/app.js
+++ api/app.js
@@ -8,7 +8,7 @@ var app = express();
 // Set up routes 
 
 const BuildingController = require('./controllers/buildingController');
-
+const buildingRoutes = require('./routes/buildingRoutes')
 //var indexRouter = require("./routes/index");
 //var usersRouter = require("./routes/users");
 //var chatgeneratorRouter = require("./routes/chatgenerator");
@@ -16,6 +16,7 @@ const BuildingController = require('./controllers/buildingController');
 
 
 app.use(cors());
+app.use('/api', buildingRoutes);
 //app.use("/", indexRouter);
 //app.use("/users", usersRouter);
 //app.use("/chatgenerator", chatgeneratorRouter);
@@ -23,5 +24,18 @@ app.use(cors());
 
 app.get('/buildings', BuildingController.getBuildingsInArea);
 //app.get('/buildings/:id', BuildingController.getBuildingDetails);
+
+const http = require('http');
+
+const port = 3000; 
+const server = http.createServer((req, res) => {
+  res.end('Hello this is the building/maps team!');
+});
+
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+// catch 404 and forward to error handler, code to be ADDED HERE.
 
 module.exports = app;
