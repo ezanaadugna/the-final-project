423b73683567425ef4ce373684cb103a2783d1ec
PegahNa <pegah_uk@yahoo.co.uk>
2023-07-26 14:32:33 +0100

    modified the test

--- api/routes/chat.js
+++ api/routes/chat.js
@@ -3,14 +3,12 @@ const router = express.Router();
 
 const ChatController = require("../controllers/chatController");
 
-router.get('/chat', ChatController.generateChat);
+router.get("/chat", ChatController.generateChat);
 
-router.get('/chat', (req, res) => {
-    res.send('Hello from the chat route!');
-  });
+// router.get('/chat', (req, res) => {
+//     res.send('Hello from the chat route!');
+//   });
 
 module.exports = router;
-
-
 
 //app.use("/chat", chatRouter);
