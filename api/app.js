981664c21cd9d820cd2480e89ff4bf220d5e82e2
heatherbrandwilliamson <heather.brandwilliamson@gmail.com>
2023-07-27 10:23:02 +0100

    update to app.js to fix routes

--- api/app.js
+++ api/app.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 
 // Set up routes 
 
-const BuildingController = require('./controllers/buildingController');
+//const BuildingController = require('./controllers/buildingController');
 const buildingRoutes = require('./routes/buildingRoutes')
 const chatRouter = require("./routes/chat");
 //const indexRouter = require("./routes/index");
@@ -16,13 +16,15 @@ const app = express();
 
 // setup for receiving JSON
 
-app.use(cors());
+
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use(cors());
+app.use("/", indexRouter);
 app.use('/buildings', buildingRoutes);
 //app.get('/buildings', BuildingController.getBuildingsInArea);
 app.use("/chat", chatRouter);
