24a7697fee4eccdfa83cce612e06e2ca4ede1e26
Ezana Adugna <eadugna3005@gmail.com>
2023-07-28 11:02:09 +0100

    splash screen now works

--- frontend/App.js
+++ frontend/App.js
@@ -2,22 +2,25 @@ import React, { useEffect } from 'react';
 import { View, StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-// import SplashScreen from 'react-native-splash-screen';
+import SplashScreen from 'react-native-splash-screen';
+import SplashScreenComponent from './screens/SplashScreen';
+// import YesScreen from './screens/YesScreen';
 import MapScreen from './screens/MapScreen';
-import * as SplashScreen from 'expo-splash-screen';
+// import * as SplashScreen from 'expo-splash-screen';
 
 const Stack = createStackNavigator();
 
 const App = () => {
-  useEffect(() => {
-    // Hide the splash screen once the app component mounts
-    SplashScreen.hide();
-  }, []);
+  // useEffect(() => {
+  //   // Hide the splash screen once the app component mounts
+  //   SplashScreen.hide();
+  // }, []);
   return (
     <View style={styles.container}>
       <NavigationContainer>
         <Stack.Navigator>
-          <Stack.Screen name="Map" component={MapScreen} />
+          <Stack.Screen name="Splash" component={SplashScreenComponent} options={{ headerShown: false }} />
+          <Stack.Screen name="MapScreen" component={MapScreen} />
         </Stack.Navigator>
       </NavigationContainer>
     </View>
---
+++ frontend/screens/SplashScreen.js
@@ -0,0 +1,43 @@
+import React, { useEffect } from 'react';
+import { View, Image, StyleSheet } from 'react-native';
+
+const SplashScreen = ({ navigation }) => {
+  useEffect(() => {
+    // Simulate some asynchronous task, e.g., fetching data or checking authentication
+    const simulateAsyncTask = () => {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          resolve();
+        }, 3000); // Replace 3000 with the time you want your splash screen to show (in milliseconds).
+      });
+    };
+
+    // Navigate to the next screen after the splash screen time has passed
+    simulateAsyncTask().then(() => {
+      navigation.replace('MapScreen'); // Replace 'MainScreen' with the name of your main app screen.
+    });
+  }, [navigation]);
+
+  return (
+    <View style={styles.container}>
+      {/* Add your splash screen image here */}
+      <Image source={require('../assets/MC.jpeg')} style={styles.image} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#180101', // Replace with your desired background color
+  },
+  image: {
+    width: '80%',
+    height: '80%',
+    resizeMode: 'contain',
+  },
+});
+
+export default SplashScreen;
