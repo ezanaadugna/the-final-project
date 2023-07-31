09396e55e539f677948658f9fed5dd3e882ecf09
PegahNa <pegah_uk@yahoo.co.uk>
2023-07-31 06:40:51 +0100

    changes the View to ScrollView

--- frontend/screens/ScrollScreen.js
+++ frontend/screens/ScrollScreen.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React from "react";
+import { ScrollView, Text, StyleSheet } from "react-native";
 
 const ScrollScreen = () => {
   return (
-    <View style={styles.container}>
+    <ScrollView style={styles.container}>
       <Text style={styles.text}>Well done!</Text>
-    </View>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#fff',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#fff",
   },
   text: {
     fontSize: 24,
