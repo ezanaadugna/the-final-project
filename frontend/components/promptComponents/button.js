85fd09cc3337f7a9d20780686a93f5ffb1875b3a
kganzorig <khusy.gan@gmail.com>
2023-08-03 11:17:23 +0100

    working now

--- frontend/components/promptComponents/button.js
+++ frontend/components/promptComponents/button.js
@@ -2,13 +2,22 @@ import React from 'react';
 import { Text, TouchableOpacity } from 'react-native';
 import PromptStyles from '../styles/promptStyles';
 
-const ButtonComponent = ({ text }) => {
-  return (
-    <TouchableOpacity style={PromptStyles.buttonCopy}>
-      <Text style={PromptStyles.buttonContent}>{ text }</Text>
-    </TouchableOpacity>
-  );
-};
-
+  const ButtonComponent = ({ text, onPress }) => {
+    const handlePress = () => {
+      console.log('Button Pressed');
+      onPress();
+    };
+    return (
+      <TouchableOpacity style={PromptStyles.buttonCopy} onPress={onPress}>
+        <Text style={PromptStyles.buttonContent}>{ text }</Text>
+      </TouchableOpacity>
+    );
+  };
 export default ButtonComponent;
+
+
+
+
+
+
 
